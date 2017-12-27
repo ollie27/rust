20cbac9fd6250afd348e65522d94f03292407a0b
Oliver Middleton <olliemail27@gmail.com>
2017-12-27 00:31:45 +0000

    rustdoc: trait impls

--- src/librustdoc/clean/mod.rs
+++ src/librustdoc/clean/mod.rs
@@ -120,7 +120,7 @@ pub struct Crate {
     pub access_levels: Arc<AccessLevels<DefId>>,
     // These are later on moved into `CACHEKEY`, leaving the map empty.
     // Only here so that they can be filtered through the rustdoc passes.
-    pub external_traits: FxHashMap<DefId, Trait>,
+    pub external_traits: FxHashMap<DefId, Item>,
     pub masked_crates: FxHashSet<CrateNum>,
 }
 
@@ -3189,8 +3189,7 @@ fn register_def(cx: &DocContext, def: Def) -> DefId {
     if did.is_local() { return did }
     inline::record_extern_fqn(cx, did, kind);
     if let TypeKind::Trait = kind {
-        let t = inline::build_external_trait(cx, did);
-        cx.external_traits.borrow_mut().insert(did, t);
+        inline::record_extern_trait(cx, did);
     }
     did
 }
